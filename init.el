05ca486fff778df7d1dbbd88622ca53c0f224e2f
Shantanu Bhardwaj <bhardwaj.10@gmail.com>
2023-06-18 11:01:36 -0700

    Config changes

--- init.el
+++ init.el
@@ -18,45 +18,55 @@
 
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3");;; Set up package
 
-;; (require 'package)
-;; (add-to-list 'package-archives
-;;              '("melpa" . "https://melpa.org/packages/") t)
-;; ;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-;; (when (boundp 'package-pinned-packages)
-;;   (setq package-pinned-packages
-;;         '((org-plus-contrib . "org"))))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(setq package-archives
+      '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+        ("MELPA"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("GNU ELPA"     . 0)
+        ("MELPA"        . 10)))
+
 ;; (package-initialize)
 
+(eval-when-compile
+  (require 'use-package))
+;; (require 'diminish)                ;; if you use :diminish
+;; (require 'bind-key)                ;; if you use any :bind variant
+;; (use-package general)			
+(use-package diminish
+  :ensure t)
+
 ;; Disable package.el in favor of straight.el
-(setq package-enable-at-startup nil)
+;; (setq package-enable-at-startup nil)
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-        'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;;       (bootstrap-version 5))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;;         'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'use-package)
+;; (straight-use-package 'use-package)
 ;;(setq straight-use-package-by-default t)
 
 ;; Configure use-package to use straight.el by default
-(use-package straight
-  ;;:custom (straight-use-package-by-default t)
-  )
-(use-package general
-  :straight t)
-(use-package diminish
-  :straight t)
-(require 'general)
-(require 'diminish)                ;; if you use :diminish
-(require 'bind-key)
+;; (use-package straight
+;;   ;;:custom (straight-use-package-by-default t)
+;;   )
+;; (use-package general)
+;; (use-package diminish)
+;; (require 'general)
+;; (require 'diminish)                ;; if you use :diminish
+;; (require 'bind-key)
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
